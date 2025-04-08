936d87002e44324d6f44ef7f0f051918d53e506f
MoonLyss <shaitan.lyssm@gmail.com>
2025-04-08 19:30:06 +0200

    Add hecate

---
+++ hecate/build/CMakeFiles/Hecate.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hecate.dir/src/main.cpp.o"
+  "CMakeFiles/Hecate.dir/src/main.cpp.o.d"
+  "Hecate"
+  "Hecate.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hecate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
