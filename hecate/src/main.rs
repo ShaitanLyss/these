c45e8fbbdd458435f5022728f7f5c96da5040e9c
MoonLyss <shaitan.lyssm@gmail.com>
2025-04-18 18:02:41 +0200

    Add dependencies and cleanup structure

--- hecate/src/main.rs
+++ hecate/src/main.rs
@@ -1,7 +1,27 @@
 use hecate;
 
+use clap::{Parser, Subcommand};
+
+/// The world ain't ready for Hecate!
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+
+    #[command(subcommand)]
+    command: Commands
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Cpp
+}
+
 fn main() {
-    unsafe {
-        println!("{}", hecate::add(1, 2));
+    let args = Args::parse();
+
+    match &args.command {
+        Commands::Cpp => unsafe {
+            println!("{}", hecate::add(-12, 24));
+        },
     }
 }
--- hecate/src/wrapper.h
+++
@@ -1,1 +0,0 @@
-#include "../cpp/src/main.h"
